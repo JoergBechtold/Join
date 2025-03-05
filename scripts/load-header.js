0f1f3629541c04017df25f4fa7c26559e232d64f
EL Houcine Ezzariba <elhoucineezzariba@MacBook-Pro-von-EL.local>
2025-03-05 02:58:37 +0100

    Header-sidebar adjusted

--- scripts/load-header.js
+++ scripts/load-header.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("header_sidebar.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("header-container").innerHTML = data;
+            document.getElementById("header_container").innerHTML = data;
         })
         .catch(error => console.error("Fehler beim Laden des Headers:", error));
 });
