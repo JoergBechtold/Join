50bba8ea7ad479e5f9be422a30c75912241e88b6
DominiqueDockal <dominique.dockal@gmail.com>
2025-03-05 14:32:16 +0100

    fixed function length Priority Buttons add task

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -2,31 +2,38 @@ let activeButton = null;
 
 function setPriority(buttonId) {
   const button = document.getElementById(buttonId);
-  
-  if (activeButton && activeButton !== button) {
-    activeButton.classList.remove('active-prio', 'red-prio', 'orange-prio', 'green-prio');
-    const prevImg = document.getElementById(activeButton.id + '_img');
-    prevImg.src = prevImg.src.replace('-event', '');
+  if (activeButton) {
+    resetActiveButton();
   }
-  
   if (activeButton !== button) {
-    activeButton = button;
-    button.classList.add('active-prio');
-    
-    const img = document.getElementById(buttonId + '_img');
-    
-    if (buttonId === 'urgent_button') {
-      button.classList.add('red-prio');
-      img.src = '/assets/icons/prio-high-event.svg';
-    } else if (buttonId === 'medium_button') {
-      button.classList.add('orange-prio');
-      img.src = '/assets/icons/prio-medium-event.svg';
-    } else if (buttonId === 'low_button') {
-      button.classList.add('green-prio');
-      img.src = '/assets/icons/prio-low-event.svg';
-    }
+    activateButton(button);
   } else {
-    activeButton = null;
+    activeButton = null; // Deaktiviert den Button, wenn er erneut geklickt wird
   }
 }
+
+function resetActiveButton() {
+  activeButton.classList.remove('active-prio', 'red-prio', 'orange-prio', 'green-prio');
+  const prevImg = document.getElementById(activeButton.id + '_img');
+  prevImg.src = prevImg.src.replace('-event', '');
+}
+
+function activateButton(button) {
+  activeButton = button;
+  button.classList.add('active-prio');
+  const buttonType = button.id.split('_')[0];
+  updateButtonStyle(buttonType);
+}
+  
+function updateButtonStyle(buttonType) {
+  const prioStyles = {
+      urgent: ['red-prio', 'high'],
+      medium: ['orange-prio', 'medium'],
+      low: ['green-prio', 'low']
+  };
+  const [className, iconType] = prioStyles[buttonType];
+  activeButton.classList.add(className);
+  document.getElementById(activeButton.id + '_img').src = `/assets/icons/prio-${iconType}-event.svg`;
+}
+
 
