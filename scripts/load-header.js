7d450c210c5746a8174c7d426863bb3c84ae7957
EL Houcine Ezzariba <elhoucineezzariba@MacBook-Pro-von-EL.local>
2025-03-06 05:44:18 +0100

    submenu

--- scripts/load-header.js
+++ scripts/load-header.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Fehler beim Laden des Headers:", error));
 });
+
+function toggleSubmenu() {
+    let submenu = document.getElementById("user-submenu");
+    submenu.classList.toggle("hidden"); 
+}
+
+document.addEventListener("click", function (event) {
+    let profile = document.querySelector(".user-profile");
+    let submenu = document.getElementById("user-submenu");
+
+    if (!profile.contains(event.target) && !submenu.contains(event.target)) {
+        submenu.classList.add("hidden");
+    }
+});
