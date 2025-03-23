b88fe7b618c0390b83d952cd557ceef3a643ecf7
DominiqueDockal <dominique.dockal@gmail.com>
2025-03-23 08:30:21 +0100

    add task priority buttons js

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -1,44 +1,4 @@
-let activeButton = null;
 
-function setPriority(buttonId) {
-  const button = document.getElementById(buttonId);
-  if (activeButton) {
-    resetActiveButton();
-  }
-  if (activeButton !== button) {
-    activateButton(button);
-  } else {
-    activeButton = null;
-  }
-}
-
-function resetActiveButton() {
-  if (activeButton) {
-    activeButton.classList.remove('active-prio', 'red-prio', 'orange-prio', 'green-prio');
-    const prevImg = document.getElementById(activeButton.id + '_img');
-    if (prevImg) {
-      prevImg.src = prevImg.src.replace('-event', '');
-    }
-  }
-}
-
-function activateButton(button) {
-  activeButton = button;
-  button.classList.add('active-prio');
-  const buttonType = button.id.split('_')[0];
-  updateButtonStyle(buttonType);
-}
-
-function updateButtonStyle(buttonType) {
-  const prioStyles = {
-    urgent: ['red-prio', 'high'],
-    medium: ['orange-prio', 'medium'],
-    low: ['green-prio', 'low'],
-  };
-  const [className, iconType] = prioStyles[buttonType];
-  activeButton.classList.add(className);
-  document.getElementById(activeButton.id + '_img').src = `/assets/icons/prio-${iconType}-event.svg`;
-}
 
 function handleSubtaskInput() {
   const addIcon = document.getElementById('add_subtask_icon');
