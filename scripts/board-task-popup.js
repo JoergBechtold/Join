d59b22e9fb14a364af92f7ae42070ce5d7bfb991
Jörg Bechtold <joergbechtold@gmx.net>
2025-03-24 14:14:39 +0100

    created close and delete and edit button in Board Task Card popup

--- scripts/board-task-popup.js
+++ scripts/board-task-popup.js
@@ -1,17 +1,19 @@
 function getAssignedHTML(task) {
-  let html = "";
-  let allContacts = JSON.parse(sessionStorage.getItem("contacts")) || {};
+  let html = '';
+  let allContacts = JSON.parse(sessionStorage.getItem('contacts')) || {};
   if (task.assigned_to) {
     let assignedArray = Object.values(task.assigned_to);
-    assignedArray.forEach(assignedItem => {
+    assignedArray.forEach((assignedItem) => {
       let allContactsArray = Object.values(allContacts);
-      let foundContact = allContactsArray.find(contactObj => {
+      let foundContact = allContactsArray.find((contactObj) => {
         return contactObj.initials === assignedItem.initials;
       });
       if (foundContact) {
         html += `
           <div class="assigned-contact-container">
-              <div class="assigned-contact" style="background-color:${foundContact.contactColor || foundContact.randomColor || assignedItem.randomColor || "#ccc"};">
+              <div class="assigned-contact" style="background-color:${
+                foundContact.contactColor || foundContact.randomColor || assignedItem.randomColor || '#ccc'
+              };">
                 <span class="contact-initials">${foundContact.initials}</span>
               </div>
               <div>
@@ -22,36 +24,38 @@ function getAssignedHTML(task) {
       }
     });
   } else {
-    html = "<span>No contacts assigned</span>";
+    html = '<span>No contacts assigned</span>';
   }
   return html;
 }
 
 function getSubtasksHTML(task) {
-  let html = "";
+  let html = '';
   if (task.subtasks) {
-    Object.values(task.subtasks).forEach(subtask => { html += `<li>${subtask}</li>`; });
+    Object.values(task.subtasks).forEach((subtask) => {
+      html += `<li>${subtask}</li>`;
+    });
   } else {
-    html = "<li>No subtasks</li>";
+    html = '<li>No subtasks</li>';
   }
   return html;
 }
 
 function getCategoryBg(task) {
-  if (task.category === "User Story") return "background-color: #0038FF;";
-  if (task.category === "Technical Task") return "background-color: #1FD7C1;";
-  return "";
+  if (task.category === 'User Story') return 'background-color: #0038FF;';
+  if (task.category === 'Technical Task') return 'background-color: #1FD7C1;';
+  return '';
 }
 
 function openPopup(taskKey) {
-  let tasks = JSON.parse(sessionStorage.getItem("tasks")),
-      task = tasks ? tasks[taskKey] : null,
-      popupContainer = document.getElementById("popupContainer"),
-      popup = document.getElementById("popup");
+  let tasks = JSON.parse(sessionStorage.getItem('tasks')),
+    task = tasks ? tasks[taskKey] : null,
+    popupContainer = document.getElementById('popupContainer'),
+    popup = document.getElementById('popup');
   if (task) {
     let assignedHTML = getAssignedHTML(task),
-        subtasksHTML = getSubtasksHTML(task),
-        categoryBackground = getCategoryBg(task);
+      subtasksHTML = getSubtasksHTML(task),
+      categoryBackground = getCategoryBg(task);
     popup.innerHTML = getPopupContent(task, assignedHTML, subtasksHTML, categoryBackground);
   } else {
     popup.innerHTML = `<div class="popup-header">
@@ -59,68 +63,73 @@ function openPopup(taskKey) {
                          <button class="close-button" onclick="closePopup()">X</button>
                        </div>`;
   }
-  popupContainer.style.display = "flex";
-  document.getElementById("overlay").style.display = "block";
+  popupContainer.style.display = 'flex';
+  document.getElementById('overlay').style.display = 'block';
 }
 
 function getPriorityIcon(priority) {
-  if (priority === "low") {
-    return "./assets/icons/prio-low.svg";
-  } else if (priority === "medium") {
-    return "./assets/icons/prio-medium.svg";
-  } else if (priority === "urgent") {
-    return "./assets/icons/prio-high.svg";
+  if (priority === 'low') {
+    return './assets/icons/prio-low.svg';
+  } else if (priority === 'medium') {
+    return './assets/icons/prio-medium.svg';
+  } else if (priority === 'urgent') {
+    return './assets/icons/prio-high.svg';
   }
 }
 
 function getPopupContent(task, assignedHTML, subtasksHTML, categoryBg) {
   const priorityIconSrc = getPriorityIcon(task.priority);
 
-  return `<div class="popup-header">
-            <div style="${categoryBg}" class="tag-container" id="tag-container">
-              <span class="tag" id="Tag">${task.category}</span>
-            </div>
-            <button class="close-button" type="button" onclick="closePopup()">
-              <img src="./assets/icons/close.png" alt="Close" class="close-icon" />
-            </button>
-          </div>
-          <div class="popup-info">
-            <h1>${task.title || "No Title"}</h1>
-            <h5 class="popup-subtitle">${task.description || "No description provided."}</h5>
-            <div class="info-item-date">
-              <span class="label">Due date:</span>
-              <span class="value">${task.due_date || "No due date"}</span>
-            </div>
-            <div id="priority-container" class="priority-container">
-              <span class="label">Priority:</span>
-              <div class="priority-lable-container">
-                <span id="priority-label">${task.priority || "No Priority"}</span>
-                <img id="priority-icon" src="${priorityIconSrc}" alt="${task.priority || "No Priority"}" />
-              </div>
-            </div>
-            <div class="info-item-assigned">
-              <span class="label">Assigned To:</span>
-              <div id="assignee-container" class="assignee-container">${assignedHTML}</div>
-            </div>
-            <div class="popup-subtasks">
-              <span class="subtasks-label">Subtasks:</span>
-              <ul class="subtasks-list" id="subtasks-list">${subtasksHTML}</ul>
-            </div>
-          </div>
-          <div class="popup-actions">
-            <button class="action-button" type="button" onclick="deleteTask()">
-              <img src="./assets/icons/delete.png" alt="Delete" class="action-icon" />
-              <span class="action-label">Delete</span>
-            </button>
-            <img src="./assets/icons/high-stroke-gray.png" alt="Separator" class="action-separator" />
-            <button class="action-button" type="button" onclick="editTask()">
-              <img src="./assets/icons/edit-black.png" alt="Edit" class="action-icon" />
-              <span class="action-label">Edit</span>
-            </button>
-          </div>`;
+  return ` <div class="popup-header">
+      <div style="${categoryBg}" class="tag-container" id="tag-container">
+        <span class="tag" id="Tag">${task.category}</span>
+      </div>
+      <button class="close-button" type="button" onclick="closePopup()">
+        <img src="./assets/icons/close.png" alt="Close" class="close-icon" />
+      </button>
+    </div>
+    <div class="popup-info">
+      <h1>${task.title || 'No Title'}</h1>
+      <h5 class="popup-subtitle">${task.description || 'No description provided.'}</h5>
+      <div class="info-item-date">
+        <span class="label">Due date:</span>
+        <span class="value">${task.due_date || 'No due date'}</span>
+      </div>
+      <div id="priority-container" class="priority-container">
+        <span class="label">Priority:</span>
+        <div class="priority-lable-container">
+          <span id="priority-label">${task.priority || 'No Priority'}</span>
+          <img id="priority-icon" src="${priorityIconSrc}" alt="${task.priority || 'No Priority'}" />
+        </div>
+      </div>
+      <div class="info-item-assigned">
+        <span class="label">Assigned To:</span>
+        <div id="assignee-container" class="assignee-container">${assignedHTML}</div>
+      </div>
+      <div class="popup-subtasks">
+        <span class="subtasks-label">Subtasks:</span>
+        <ul class="subtasks-list" id="subtasks-list">
+          ${subtasksHTML}
+        </ul>
+      </div>
+    </div>
+    <div class="popup-actions">
+      <div class="action-box delete" onclick="deleteTask()">
+        <div class="delete-icon">
+          <img src="assets/icons/paperbasketdelet.svg" alt="Delete" id="delete_icon" />
+        </div>
+        <span class="delete-btn">Delete</span>
+      </div>
+      <div class="action-box edit" onclick="editTask()">
+        <div class="edit-icon">
+          <img src="assets/icons/edit-black.png" alt="Edit" id="edit_icon" />
+        </div>
+        <span class="edit-btn">Edit </span>
+      </div>
+    </div> `;
 }
 
 function closePopup() {
-  document.getElementById("popupContainer").style.display = "none";
-  document.getElementById("overlay").style.display = "none";
+  document.getElementById('popupContainer').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
 }
