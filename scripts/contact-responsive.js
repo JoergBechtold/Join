f85a9407dbe2440b2f422dc6444d5f92fbdf9272
EL Houcine Ezzariba <elhoucineezzariba@MacBook-Pro-von-EL.local>
2025-04-09 02:52:40 +0200

    contact detail adjusted

--- scripts/contact-responsive.js
+++ scripts/contact-responsive.js
@@ -1,9 +1,10 @@
 function showContactDetail() {
     if (window.innerWidth <= 768) {
-        document.querySelector('.main-content-responsive').style.display = 'flex';
+        document.querySelector('.main-content').style.display = 'flex';
         document.querySelector('.contact-list').style.display = 'none';
         document.querySelector('.sidebarContact').style.display = 'none';
-        // document.querySelector('.contact-detail').style.display = 'flex';
+        document.querySelector('.contact-detail').style.display = 'flex';
+        document.querySelector('.original-h1').style.display = 'none';
       }
   }
 
@@ -12,7 +13,8 @@ function showContactDetail() {
         document.querySelector(".contacts-container").style.display = "flex";
         document.querySelector('.sidebarContact').style.display = 'flex';
         document.querySelector(".contact-list").style.display = "flex";
-        document.querySelector(".main-content-responsive").style.display = "none";
+        document.querySelector(".main-content").style.display = "none";
         document.querySelector(".contact-detail").style.display = "none";
+        document.querySelector('.original-h1').style.display = 'flex';
   }
 }
